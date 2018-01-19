bb50764024f847dd5519b51984f23388d04e89c4
Gerson Noboa <gerson.noboa@gmail.com>
2018-01-20 01:39:02 +0200

    Changed upload folder to ./uploads instead of root

--- hello.py
+++ hello.py
@@ -6,12 +6,15 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 CORS(app)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-#UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads')
-app.config['UPLOAD_FOLDER'] = APP_ROOT
+UPLOAD_PATH = os.path.join(APP_ROOT, 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_PATH
 ALLOWED_EXTENSIONS = set(["csv", "xes", "mxml", "txt", "xml"])
 
+def get_extension(filename):
+    return filename.rsplit('.', 1)[1].lower()
+
 def allowed_filename(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and get_extension(filename) in ALLOWED_EXTENSIONS
  
 @app.route("/hello")
 def hello():
@@ -21,15 +24,21 @@ def hello():
 def upload_file():
     if ('file' not in request.files):
         return 'No file has been uploaded'
+
     file = request.files['file']
     if file.filename == '':
         return 'No file has been uploaded'
+
     if file and allowed_filename(file.filename):        
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], "data." + get_extension(filename)))
         return 'File uploaded successfully'
     else:
         return 'Error uploading file' + str(file) + ' ' + str(allowed_filename(file.filename))
-    
+
+@app.route("/active-time", methods=['GET'])
+def get_active_time_info():
+    return 'Not implemented yet'
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
